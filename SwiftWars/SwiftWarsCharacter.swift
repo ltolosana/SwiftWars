dbefc752974a1398538f9d91dc7102977f554fee
Luis M Tolosana Simon <Luisma@MacBook-Pro-de-Luisma.local>
2015-05-09 17:38:46 +0200

    Añadida la clase SwiftWarsUniverse, pero no puedo cargar el audio ni la foto

--- SwiftWars/SwiftWarsCharacter.swift
+++ SwiftWars/SwiftWarsCharacter.swift
@@ -14,9 +14,9 @@ class SwiftWarsCharacter {
     
     var name: String = ""
     var alias: String = ""
-    var wikiURL: NSURL?
-    var soundData: NSData?
-    var photo: UIImage?
+    var wikiURL: Optional<NSURL>
+    var soundData: Optional<NSData>
+    var photo: Optional<UIImage>
     
     // Inits
     init () {}
@@ -27,6 +27,67 @@ class SwiftWarsCharacter {
         self.wikiURL = wikiURL
         self.soundData = soundData
         self.photo = photo
+    }
+    
+    convenience init (alias: String, wikiURL: NSURL?, soundData: NSData?, photo: UIImage?){
+        self.init()
+        self.alias = alias
+        self.wikiURL = wikiURL
+        self.soundData = soundData
+        self.photo = photo
+    }
+}
+    
+class SwiftWarsUniverse {
+    
+    var rebels: Array<SwiftWarsCharacter>
+    var imperials : Array<SwiftWarsCharacter>
+    
+    // Inits
+    init () {
+        
+        // Creo los personajes
+        
+        // VADER
+        let vaderURL = NSURL(string: "http://en.wikipedia.org/wiki/Darth_Vader")
+        
+        let path = NSBundle.mainBundle().URLForResource("SwiftWars/vader", withExtension: "caf")
+        let vaderSoundData2 = NSData(contentsOfURL: path!)
+        let vaderSoundData = NSData(contentsOfFile: "vader.caf")
+
+        
+        // OJO REVISAR ESTO QUE LO HE PUESTO EN PMODO SUICIDA
+//        let vaderPhotoData = NSData(contentsOfFile: "darthVader.jpg")
+        let vaderPhotoImage = UIImage(named: "darthVader.jpg")
+        
+            
+        let vader = SwiftWarsCharacter(name: "Anakin Skywalker", alias: "Darth Vader", wikiURL: vaderURL, soundData: vaderSoundData, photo: vaderPhotoImage)
+        
+        
+        // VADER
+        let chewieURL = NSURL(string: "http://en.wikipedia.org/wiki/Chewbacca")
+        
+        let chewieSoundData = NSData(contentsOfFile: "chewbacca.caf")
+        
+        // OJO REVISAR ESTO QUE LO HE PUESTO EN PMODO SUICIDA
+//        let chewiePhotoData = NSData(contentsOfFile: "chewbacca.jpg")
+        let chewiePhotoImage = UIImage(named: "chewbacca.jpg")
+        
+        
+        let chewie = SwiftWarsCharacter(alias: "Chewbacca", wikiURL: chewieURL, soundData: chewieSoundData, photo: chewiePhotoImage)
+
+        
+        rebels = [chewie]
+        imperials = [vader]
+        
+    }
+    
+    func rebelsCount() -> Int{
+        return rebels.count
+    }
+    
+    func imperialsCount() -> Int{
+        return imperials.count
     }
     
 }
